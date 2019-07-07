ed6d5160e031e311266811f74588d93c9d432f73
Tushar Mehndiratta <27002169+Invisible786@users.noreply.github.com>
2019-07-07 18:52:22 +0530

    Python Programs Added for Motion Detector

---
+++ motion_detector.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# coding: utf-8
+# Coded by Tushar Mehndiratta(Invisible786)
+# https://github.com/Invisible786
+
+import cv2, time, pandas
+from datetime import datetime
+
+first_frame = None
+status_list = [None,None]
+times = []
+
+#DataFrame to Store the time Values during which object detection and movement appears
+df=pandas.DataFrame(columns=["Start","End"])
+
+video = cv2.VideoCapture(0)
+
+while True:
+    check, frame = video.read()
+    # Status at the begning of the recording is zero as the object is not visible
+    status = 0
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray,(21,21),0)
+
+    # To store first image/frame of video
+    if first_frame is None:
+        first_frame=gray
+        continue
+     
+    # Calculate the difference between the first frame and other frames
+    delta_frame=cv2.absdiff(first_frame,gray)
+
+    # Provides a threshold value, such that it will convert 
+    # the difference value with less than 30 to black.
+    # If the difference is greater than 30 it will convert 
+    # those pixels to white
+    thresh_frame=cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
+    thresh_frame=cv2.dilate(thresh_frame, None, iterations=2)
+     
+     # Define the contour area.
+    # Basically, add the borders.
+    (cnts,_)=cv2.findContours(thresh_frame.copy(),cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Remove noises and shadows. Basically it will keep only that 
+    # part white, which has area greater than 1000 pixels.
+    for contour in cnts:
+        if cv2.contourArea(contour) < 10000:
+            continue
+        # Status changes when object becomes visible
+        status=1
+
+        # Creates a rectangular box around the object in the frame
+        (x, y, w, h)=cv2.boundingRect(contour)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0,255,0), 3)
+    status_list.append(status)
+
+    status_list=status_list[-2:]
+
+    # Record datetime in a list when change occurs
+    if status_list[-1]==1 and status_list[-2]==0:
+        times.append(datetime.now())
+    if status_list[-1]==0 and status_list[-2]==1:
+        times.append(datetime.now())
+
+
+    cv2.imshow("Gray Frame",gray)
+    cv2.imshow("Delta Frame",delta_frame)
+    cv2.imshow("Threshold Frame",thresh_frame)
+    cv2.imshow("Color Frame",frame)
+
+    key=cv2.waitKey(1)
+
+    if key == ord('q'):
+        if status == 1:
+            times.append(datetime.now())
+        break
+
+print(status_list)
+print(times)
+
+for i in range(0, len(times), 2):
+    # Store time values in DataFrame
+    df = df.append({"Start": times[i],"End": times[i+1]}, ignore_index=True)
+
+# Write a DataFrame to a CSV file
+df.to_csv("Times.csv")
+
+video.release()
+cv2.destroyAllWindows
+
+
